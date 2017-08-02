c253cc492367e5da1fcceb1d5c78d3d6cdcb7475
Vladimir__Lipodat <sibi_7@mail.ru>
2017-08-02 17:25:24 +0300

    fix bug

--- app/js/script.js
+++ app/js/script.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
     $('.owl-carousel').owlCarousel({
-        margin: 0,
-        dots: true,
+        margin: 12,
+        dots: false,
+        nav:true,
+        navText: ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
         responsive:{
             0:{
                 items:1,
@@ -13,8 +15,9 @@ $(document).ready(function(){
             },
             1000:{
                 items:3,
-                dots: true,
-                loop:true
+                dots: false,
+                loop:true,
+                nav:true
             }
         }
     });
@@ -28,4 +31,67 @@ $(document).ready(function(){
         $( ".header__menu" ).slideToggle( "slow", function() {
         });
     });
+
+    (function($){
+        jQuery.fn.lightTabs = function(options){
+
+            var createTabs = function(){
+                tabs = this;
+                i = 0;
+
+                showPage = function(i){
+                    $(tabs).children("div").children("div").hide();
+                    $(tabs).children("div").children("div").eq(i).show();
+                    $(tabs).children("ul").children("li").removeClass("active");
+                    $(tabs).children("ul").children("li").eq(i).addClass("active");
+                }
+
+                showPage(0);
+
+                $(tabs).children("ul").children("li").each(function(index, element){
+                    $(element).attr("data-page", i);
+                    i++;
+                });
+
+                $(tabs).children("ul").children("li").click(function(){
+                    showPage(parseInt($(this).attr("data-page")));
+                });
+            };
+            return this.each(createTabs);
+        };
+    })(jQuery);
+    $(document).ready(function(){
+        $(".tabs").lightTabs();
+    });
+    $("#menus").on("click","a", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+
+        //забираем идентификатор бока с атрибута href
+        var id  = $(this).attr('href'),
+
+            //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+    $("#menuu").on("click","a", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+
+        //забираем идентификатор бока с атрибута href
+        var id  = $(this).attr('href'),
+
+            //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+
+    $('.tabs ul li').click(function () {
+        $('.tabs ul li').addClass('active')
+
+    })
 });
